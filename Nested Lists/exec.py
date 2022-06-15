6f877f06ac612f39c5c6238a0dab7617b19dfcc7
Vitor Almeida <tikalleu2005843@gmail.com>
2022-06-14 23:36:04 -0300

    Nested Lists

---
+++ Nested Lists/exec.py
@@ -0,0 +1,10 @@
+
+records = {}
+if __name__ == '__main__':
+    for _ in range(int(input())):
+        name = input()
+        score = float(input())
+       
+        records.update({name: score})
+
+    min(records.values())
